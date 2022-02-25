01dec5c43d02e5c27c165d1c852f0f7632cab876
oew1g21 <oew1g21@soton.ac.uk>
2022-02-25 23:31:21 +0000

    Script now deletes every heading by only printing records that are divisible by 2

--- cw1_material/create_csv.sh
+++ cw1_material/create_csv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -o '<td>.*</td>' "$1" | sed -e 's/\(<td>\|<\/td>\| <B>\|<\/B>\|;.*\)//g' -e '/hr/d' 
+grep -o '<td>.*</td>' "$1" | sed -e 's/\(<td>\|<\/td>\| <B>\|<\/B>\|;.*\)//g' -e '/hr/d' | awk '(NR%2==0) {print}'
