37ee1d05320f4ba07402aaeeb7714edc203a08b5
oew1g21 <oew1g21@soton.ac.uk>
2022-02-26 03:04:51 +0000

    Script has been edited to concatenate data. It merges 4 lines into 1 line and uses commas to separate them. The final output is then written to the output file along with the header line.

--- cw1_material/create_csv.sh
+++ cw1_material/create_csv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -o '<td>.*</td>' "$1" | sed -e 's/\(<td>\|<\/td>\| <B>\|<\/B>\|;.*\)//g' -e '/hr/d' | awk '(NR%2==0) {print}'
+grep -o '<td>.*</td>' "$1" | sed -e 's/\(<td>\|<\/td>\| <B>\|<\/B> \|;.*\)//g' -e '/hr/d' -e's/\(, \)/,/g' | awk '(NR%2==0) {print}' | awk '{line=line "," $0}  (NR%4==0) {print substr(line,2); line=""}'>"$2" | sed -i '1 i\Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity' "$2"
