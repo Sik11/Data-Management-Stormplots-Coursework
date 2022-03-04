bccd2b188cce5feed3d4e502a241d73e5d83ac19
oew1g21 <oew1g21@soton.ac.uk>
2022-03-04 00:10:12 +0000

    Bash script has been modified so the first program in the script, gets the contents first program gets the contents inbetween the td tags. As a result, script no longer has to remove tags with sed.

--- cw1_material/create_csv.sh
+++ cw1_material/create_csv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -o '<td>.*</td>' "$1" | sed -e 's/\(<td>\|<\/td>\| <B>\|<\/B> \|;.*\)//g' -e '/hr/d' -e's/\(, \)/,/g' | awk '(NR%2==0) {print}' | awk '{line=line "," $0}  (NR%4==0) {print substr(line,2); line=""}'>"$2" | sed -i '1 i\Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity' "$2"
+awk -F "</*td>|</*tr>" '/<td>.*/ {print $3}' "$1" | sed -e 's/\(<td>\|<\/td>\| <B>\|<\/B> \|;.*\)//g' -e '/hr/d' -e's/\(, \)/,/g' | awk '(NR%2==0) {print}' | awk '{line=line "," $0}  (NR%4==0) {print substr(line,2); line=""}'>"$2" | sed -i '1 i\Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity' "$2"
