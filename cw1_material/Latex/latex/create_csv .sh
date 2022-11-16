447fd0a4722e69c34e21f4571421bd2dfefbe8c0
Sik11 <109830757+Sik11@users.noreply.github.com>
2022-11-16 16:30:14 +0000

    Delete cw1_material directory

--- cw1_material/Latex/latex/create_csv .sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk -F "</*td>|</*tr>" '/<td>.*/ {print $3}' "$1" | sed -e 's/\(.<B>\|<\/B>.\|;.*\)//g' -e '/hr/d' -e's/\(, \)/,/g'| awk '(NR%2==0) {print}' | awk '{line=line "," $0}  (NR%4==0) {print substr(line,2); line=""}'>"$2" | sed -i '1 i\Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity' "$2"
--- cw1_material/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk -F "</*td>|</*tr>" '/<\/*t[rd]>.*[A-Za-z]/ {print $3}' "$1" 
